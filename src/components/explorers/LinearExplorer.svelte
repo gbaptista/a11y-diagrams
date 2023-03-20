06271b1da02b90f97447bb83db16bed0cc449338
gbaptista <guilhermebaptistasilva@gmail.com>
2023-03-20 18:52:48 -0300

    exploring

--- src/components/explorers/LinearExplorer.svelte
+++ src/components/explorers/LinearExplorer.svelte
@@ -2,13 +2,13 @@
     export let graph;
   
     let focusNodeId = Object.keys(graph.nodes)[0];
-    let edges = graph.nodes[focusNodeId].edges;
+    let previousNodeId = undefined;
 
     let selectedNode = undefined;
   
     const goTo = (newFocusNodeId) => {
-      focusNodeId = newFocusNodeId;
-      edges = graph.nodes[focusNodeId].edges;
+        previousNodeId = focusNodeId;
+        focusNodeId = newFocusNodeId;
     };
 
     const handleKeyDown = (event) => {
@@ -19,9 +19,10 @@
         // case 'ArrowDown':
         //     // if(possibleNodeIndex > (0)) possibleNodeIndex += 1;
         //     break;
-        // case 'ArrowLeft':
-        //     console.log('Seta para esquerda pressionada');
-        //     break;
+        case 'ArrowLeft':
+            console.log('ArrowLeft', previousNodeId);
+            goTo(previousNodeId);
+            break;
         case 'ArrowRight':
            goTo(selectedNode);
             break;
@@ -29,19 +30,26 @@
             break;
         }
     }
+
+    const edgesWithoutPrevious = (focusNodeId) => {
+        const edges = graph.nodes[focusNodeId].edges;
+        return Object.keys(edges).filter((edgeId) => {
+            return edgeId != previousNodeId;
+        });
+    }
   </script>
   
   {#if graph}
     <div class="card">
       <h5 class="card-header">Linear Explorer</h5>
       <div class="card-body">
-        <label for="edges">{graph.nodes[focusNodeId].label}</label>
+        <label for="edges" aria-live="polite">{graph.nodes[focusNodeId].label}</label>
         <select bind:value={selectedNode} aria-live="polite" on:keydown={handleKeyDown} id="edges" class="form-select" >
-            {#each Object.keys(edges) as edgeId}
+            {#each edgesWithoutPrevious(focusNodeId) as edgeId}
                 <option value={edgeId}>{graph.nodes[edgeId].label}</option>
             {/each}
         </select>
-
+        {previousNodeId}
       </div>
     </div>
   {/if}
