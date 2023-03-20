612d5e3a98b91e79a91d2e4b28683f25372ec906
gbaptista <guilhermebaptistasilva@gmail.com>
2023-03-20 18:04:58 -0300

    adding focus

--- src/components/explorers/LinearExplorer.svelte
+++ src/components/explorers/LinearExplorer.svelte
@@ -9,44 +9,10 @@
   </script>
   
   {#if graph}
-    <div class="card">
+    <div class="card" tabindex="0">
       <h5 class="card-header">Linear Explorer</h5>
       <div class="card-body">
-        <div class="row">
-          <div class="col">
-            <table class="table">
-              <tbody>
-                <tr>
-                  <th class="text-end" scope="row">Label:</th>
-                  <td>{graph.nodes[focusNodeId].label}</td>
-                </tr>
-                <tr>
-                  <th class="text-end" scope="row">Edges:</th>
-                  <td>{Object.keys(graph.nodes[focusNodeId].edges).length}</td>
-                </tr>
-                <tr>
-                  <th class="text-end" scope="row">ID:</th>
-                  <td>{graph.nodes[focusNodeId].id}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div class="col">
-            {#each Object.keys(graph.nodes[focusNodeId].edges) as id}
-              <button
-                on:click={goTo(id)}
-                type="button"
-                class={`${
-                  graph.nodes[focusNodeId].edges[id]['direction'] == '>'
-                    ? 'btn btn-primary'
-                    : 'btn btn-secondary'
-                }`}
-              >
-                {graph.nodes[id].label}
-              </button>
-            {/each}
-          </div>
-        </div>
+        
       </div>
     </div>
   {/if}
