5af59179689c5fed2e5c8440b5b5db18d312d24b
gbaptista <guilhermebaptistasilva@gmail.com>
2023-03-20 18:17:35 -0300

    exploring

--- src/components/explorers/LinearExplorer.svelte
+++ src/components/explorers/LinearExplorer.svelte
@@ -6,13 +6,36 @@
     const goTo = (newFocusNodeId) => {
       focusNodeId = newFocusNodeId;
     };
+
+    const handleKeyDown = (event) => {
+        const edges = Object.keys(graph.nodes[focusNodeId].edges);
+
+        switch (event.key) {
+        case 'ArrowUp':
+            goTo(edges[0]);
+            break;
+        case 'ArrowDown':
+            goTo(edges[0]);
+            break;
+        case 'ArrowLeft':
+            console.log('Seta para esquerda pressionada');
+            break;
+        case 'ArrowRight':
+            console.log('Seta para direita pressionada');
+            break;
+        default:
+            break;
+        }
+    }
   </script>
   
   {#if graph}
     <div class="card">
       <h5 class="card-header">Linear Explorer</h5>
       <div class="card-body">
-        <button tabindex="0" type="button" class="btn btn-primary">Linear Explorer</button>
+        <button on:keydown={handleKeyDown} tabindex="0" type="button" class="btn btn-primary">
+            {graph.nodes[focusNodeId].label}
+        </button>
       </div>
     </div>
   {/if}
