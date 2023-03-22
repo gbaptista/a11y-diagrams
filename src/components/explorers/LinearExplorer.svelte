0435c9ae230c51f68dab004a4b37e9b8f2ffc899
Leonardo Gleison <leonardo.gleison@nubank.com.br>
2023-03-22 09:49:51 -0300

    fix history navigator

--- src/components/explorers/LinearExplorer.svelte
+++ src/components/explorers/LinearExplorer.svelte
@@ -1,60 +1,88 @@
 <script>
-    export let graph;
-  
-    let focusNodeId = Object.keys(graph.nodes)[0];
-    let previousNodeId = undefined;
+  export let graph;
 
-    let selectedNode = undefined;
-  
-    const goTo = (newFocusNodeId) => {
-        previousNodeId = focusNodeId;
-        focusNodeId = newFocusNodeId;
-    };
+  let focusNodeId = Object.keys(graph.nodes)[0];
+  let navHistory = [Object.keys(graph.nodes)[0]];
 
-    const handleKeyDown = (event) => {
-        switch (event.key) {
-        // case 'ArrowUp':
-        //     // if(possibleNodeIndex > 0) possibleNodeIndex -= 1;
-        //     break;
-        // case 'ArrowDown':
-        //     // if(possibleNodeIndex > (0)) possibleNodeIndex += 1;
-        //     break;
-        case 'ArrowLeft':
-            goTo(previousNodeId);
-            break;
-        case 'ArrowRight':
-           goTo(selectedNode);
-            break;
-        default:
-            break;
-        }
+  let selectedNode = undefined;
+
+  const goTo = (newFocusNodeId) => {
+    if (isValidNextNode(newFocusNodeId)) {
+      navHistory.push(newFocusNodeId);
+      focusNodeId = newFocusNodeId;
     }
+  };
 
-    const edgesWithoutPrevious = (focusNodeId) => {
-        const edges = graph.nodes[focusNodeId].edges;
-        return Object.keys(edges).filter((edgeId) => {
-            return edgeId != previousNodeId;
-        });
+  const back = () => {
+    let previousNodeId = getPreviousNode();
+    goTo(previousNodeId);
+    navHistory.pop();
+    navHistory.pop();
+  };
+
+  const getPreviousNode = () => {
+    return navHistory[navHistory.length - 2];
+  };
+
+  const isValidNextNode = (newFocusNodeId) => {
+    if (newFocusNodeId == undefined) {
+      //alert (newFocusNodeId);
+      return false;
     }
-  </script>
-  
-  {#if graph}
-    <div class="card">
-      <h5 class="card-header">Linear Explorer</h5>
-      <div class="card-body">
-        <label for="edges" aria-label="{graph.nodes[focusNodeId].label} node. Select direction">{graph.nodes[focusNodeId].label}</label>
-        <select bind:value={selectedNode} on:keydown={handleKeyDown} id="edges" class="form-select" >
-            {#each edgesWithoutPrevious(focusNodeId) as edgeId}
-                <option value={edgeId}>{graph.nodes[edgeId].label}</option>
-            {/each}
-        </select>
-      </div>
+    return true;
+  };
+
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      // case 'ArrowUp':
+      //     // if(possibleNodeIndex > 0) possibleNodeIndex -= 1;
+      //     break;
+      // case 'ArrowDown':
+      //     // if(possibleNodeIndex > (0)) possibleNodeIndex += 1;
+      //     break;
+      case 'ArrowLeft':
+        back();
+        break;
+      case 'ArrowRight':
+        goTo(selectedNode);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const edgesWithoutPrevious = (focusNodeId) => {
+    const edges = graph.nodes[focusNodeId].edges;
+    return Object.keys(edges).filter((edgeId) => {
+      return edgeId != getPreviousNode();
+    });
+  };
+</script>
+
+{#if graph}
+  <div class="card">
+    <h5 class="card-header">Linear Explorer</h5>
+    <div class="card-body">
+      <label for="edges" aria-label="{graph.nodes[focusNodeId].label} node. Select direction"
+        >{graph.nodes[focusNodeId].label}</label
+      >
+      <select bind:value={selectedNode} on:keydown={handleKeyDown} id="edges" class="form-select">
+        {#each edgesWithoutPrevious(focusNodeId) as edgeId}
+          <option value={edgeId}>{graph.nodes[edgeId].label}</option>
+        {/each}
+      </select>
+      <label for="history">Navigation History</label>
+      <select id="history">
+        {#each navHistory as edgeId}
+          <option>{graph.nodes[edgeId].label}</option>
+        {/each}
+      </select>
     </div>
-  {/if}
-  
-  <style>
-    .card {
-      margin-top: 2em;
-    }
-  </style>
-  
+  </div>
+{/if}
+
+<style>
+  .card {
+    margin-top: 2em;
+  }
+</style>
