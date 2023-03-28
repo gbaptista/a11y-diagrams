a1ac4b7187ffc2512ca26dbdce6d1a2a5ea45873
gbaptista <guilhermebaptistasilva@gmail.com>
2023-03-28 15:56:22 -0300

    instructions

--- src/components/explorers/LinearExplorer.svelte
+++ src/components/explorers/LinearExplorer.svelte
@@ -14,7 +14,6 @@
   };
 
   const goTo = (newFocusNodeId) => {
-    console.log('goTo', newFocusNodeId);
     if (isValidNextNode(newFocusNodeId)) {
       navHistory.push(newFocusNodeId);
       focusNodeId = newFocusNodeId;
